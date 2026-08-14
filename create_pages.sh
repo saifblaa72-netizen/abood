74848b412e625e8e3f93b8aa1df6f54e1a9b2811
abood Alqasem <saifblaa72@gmail.com>
2026-08-14 22:42:31 +0300

    Delete create_pages.sh

--- create_pages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create all remaining component and page files for Waheeba Fashion
-
-# This script will be executed to create multiple files efficiently
-echo "Creating frontend files..."
-
-# The files will be created in the next step
